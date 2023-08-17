bd4b36beab38c0bed51932cec3b43c7e9c0ec9d8
Yaika3 <kabinidi@mail.ru>
2023-08-17 10:50:34 +0300

    Схемы бд

---
+++ src/main/java/com/example/hogwarts/scripts421.sql
@@ -0,0 +1,16 @@
+CREATE TABLE student (
+	id INTEGER PRIMARY KEY;
+	age INTEGER;
+	name TEXT;
+    ADD CONSTRAINT age_constraint CHECK (age > 16);
+	ADD CONSTRAINT age_constraint DEFAULT 20;
+	name TEXT NOT NULL,
+	name TEXT UNIQUE
+	
+);
+
+CREATE TABLE faculty (
+id INTEGER PRIMARY KEY;
+name TEXT;
+colour TEXT;
+)
