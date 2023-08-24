1abce8ee7d3ce69d93b00a1a8a5053e8636b71d2
Yaika3 <kabinidi@mail.ru>
2023-08-24 19:57:14 +0300

    Логи

--- src/main/java/com/example/hogwarts/service/StudentService.java
+++ src/main/java/com/example/hogwarts/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.hogwarts.service;
 
 import com.example.hogwarts.model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.example.hogwarts.repositories.StudentRepository;
 
@@ -10,12 +12,14 @@ import java.util.List;
 @Service
 public class StudentService {
     private StudentRepository studentRepository;
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
     public Student add(Student student){
-        return studentRepository.save(student);
+
+        logger.info("Метод add запущен");return studentRepository.save(student);
     }
     public Student get(long id){
         return studentRepository.getById(id);
